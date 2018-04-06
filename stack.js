5d6cd79c614c222fe23beb7598d8632ef8121b84
Dylan <nielsen.dylan@gmail.com>
2018-04-06 07:56:29 -0700

    finish peek

--- stack.js
+++ stack.js
@@ -17,6 +17,31 @@ class Stack {
       this.top = new _StackNode(value, null);
       return;
     }
-    
+    this.top = new _StackNode(value, this.top);
+  }
+
+  pop() {
+    if (!this.top) return null;
+    let pop = this.top;
+    this.top = this.top.next;
+    return pop.value;
   }
 }
+
+function peek(stack) {
+  return stack.top.value;
+}
+
+function main() {
+  let starTrek = new Stack();
+  starTrek.push('Kirk');
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+  console.log(peek(starTrek));
+  console.log(JSON.stringify(starTrek, null, 4));
+}
+
+main();
+
+module.exports = Stack;
